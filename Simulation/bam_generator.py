2de30c3889760e953faf61ce0f71ee0e7bc7da6f
Carlos D. Wang <dolittle007@gmail.com>
2020-03-07 17:10:04 -0600

    Add files via upload

---
+++ Simulation/bam_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#===============================================================================
+import os
+import glob
+
+for i in glob.iglob('*.read1.fastq'):
+    name = os.path.basename(i).split('.')[0]
+    r1 = i
+    r2 = '{}.read2.fastq'.format(name)
+    print('bwa mem -M -R \"@RG\\tID:{2}\\tLB:lib1\\tPL:illumina\\tSM:{2}\\tPU:unit1\" -t 8 /home/tywang/database/genome/hg19.fa {0} {1} | samtools sort -@ 8 -O BAM -o {2}.bwa.bam - && picard MarkDuplicates I={2}.bwa.bam O={2}.bam M={2}.marked_dup_metrics.txt REMOVE_DUPLICATES=true ASSUME_SORTED=true VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true && rm -rf {2}.bwa.bam'.format(r1, r2, name))
---
+++ Simulation/fastq_vaf50.sh
@@ -0,0 +1,36 @@
+dwgsim -C 50 -1 75 -2 75 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.fa 75bp_100x_WT
+dwgsim -C 50 -1 75 -2 75 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.TDUP.fa 75bp_100x_MT
+cat 75bp_100x_WT.bwa.read1.fastq 75bp_100x_MT.bwa.read1.fastq > 75bp_100x.read1.fastq && cat 75bp_100x_WT.bwa.read2.fastq 75bp_100x_MT.bwa.read2.fastq > 75bp_100x.read2.fastq && rm -rf 75bp_100x_WT.bwa.read1.fastq 75bp_100x_MT.bwa.read1.fastq 75bp_100x_WT.bwa.read2.fastq 75bp_100x_MT.bwa.read2.fastq
+dwgsim -C 25 -1 75 -2 75 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.fa 75bp_50x_WT
+dwgsim -C 25 -1 75 -2 75 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.TDUP.fa 75bp_50x_MT
+cat 75bp_50x_WT.bwa.read1.fastq 75bp_50x_MT.bwa.read1.fastq > 75bp_50x.read1.fastq && cat 75bp_50x_WT.bwa.read2.fastq 75bp_50x_MT.bwa.read2.fastq > 75bp_50x.read2.fastq && rm -rf 75bp_50x_WT.bwa.read1.fastq 75bp_50x_MT.bwa.read1.fastq 75bp_50x_WT.bwa.read2.fastq 75bp_50x_MT.bwa.read2.fastq
+dwgsim -C 10 -1 75 -2 75 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.fa 75bp_20x_WT
+dwgsim -C 10 -1 75 -2 75 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.TDUP.fa 75bp_20x_MT
+cat 75bp_20x_WT.bwa.read1.fastq 75bp_20x_MT.bwa.read1.fastq > 75bp_20x.read1.fastq && cat 75bp_20x_WT.bwa.read2.fastq 75bp_20x_MT.bwa.read2.fastq > 75bp_20x.read2.fastq && rm -rf 75bp_20x_WT.bwa.read1.fastq 75bp_20x_MT.bwa.read1.fastq 75bp_20x_WT.bwa.read2.fastq 75bp_20x_MT.bwa.read2.fastq
+dwgsim -C 50 -1 100 -2 100 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.fa 100bp_100x_WT
+dwgsim -C 50 -1 100 -2 100 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.TDUP.fa 100bp_100x_MT
+cat 100bp_100x_WT.bwa.read1.fastq 100bp_100x_MT.bwa.read1.fastq > 100bp_100x.read1.fastq && cat 100bp_100x_WT.bwa.read2.fastq 100bp_100x_MT.bwa.read2.fastq > 100bp_100x.read2.fastq && rm -rf 100bp_100x_WT.bwa.read1.fastq 100bp_100x_MT.bwa.read1.fastq 100bp_100x_WT.bwa.read2.fastq 100bp_100x_MT.bwa.read2.fastq
+dwgsim -C 25 -1 100 -2 100 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.fa 100bp_50x_WT
+dwgsim -C 25 -1 100 -2 100 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.TDUP.fa 100bp_50x_MT
+cat 100bp_50x_WT.bwa.read1.fastq 100bp_50x_MT.bwa.read1.fastq > 100bp_50x.read1.fastq && cat 100bp_50x_WT.bwa.read2.fastq 100bp_50x_MT.bwa.read2.fastq > 100bp_50x.read2.fastq && rm -rf 100bp_50x_WT.bwa.read1.fastq 100bp_50x_MT.bwa.read1.fastq 100bp_50x_WT.bwa.read2.fastq 100bp_50x_MT.bwa.read2.fastq
+dwgsim -C 10 -1 100 -2 100 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.fa 100bp_20x_WT
+dwgsim -C 10 -1 100 -2 100 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.TDUP.fa 100bp_20x_MT
+cat 100bp_20x_WT.bwa.read1.fastq 100bp_20x_MT.bwa.read1.fastq > 100bp_20x.read1.fastq && cat 100bp_20x_WT.bwa.read2.fastq 100bp_20x_MT.bwa.read2.fastq > 100bp_20x.read2.fastq && rm -rf 100bp_20x_WT.bwa.read1.fastq 100bp_20x_MT.bwa.read1.fastq 100bp_20x_WT.bwa.read2.fastq 100bp_20x_MT.bwa.read2.fastq
+dwgsim -C 50 -1 150 -2 150 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.fa 150bp_100x_WT
+dwgsim -C 50 -1 150 -2 150 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.TDUP.fa 150bp_100x_MT
+cat 150bp_100x_WT.bwa.read1.fastq 150bp_100x_MT.bwa.read1.fastq > 150bp_100x.read1.fastq && cat 150bp_100x_WT.bwa.read2.fastq 150bp_100x_MT.bwa.read2.fastq > 150bp_100x.read2.fastq && rm -rf 150bp_100x_WT.bwa.read1.fastq 150bp_100x_MT.bwa.read1.fastq 150bp_100x_WT.bwa.read2.fastq 150bp_100x_MT.bwa.read2.fastq
+dwgsim -C 25 -1 150 -2 150 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.fa 150bp_50x_WT
+dwgsim -C 25 -1 150 -2 150 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.TDUP.fa 150bp_50x_MT
+cat 150bp_50x_WT.bwa.read1.fastq 150bp_50x_MT.bwa.read1.fastq > 150bp_50x.read1.fastq && cat 150bp_50x_WT.bwa.read2.fastq 150bp_50x_MT.bwa.read2.fastq > 150bp_50x.read2.fastq && rm -rf 150bp_50x_WT.bwa.read1.fastq 150bp_50x_MT.bwa.read1.fastq 150bp_50x_WT.bwa.read2.fastq 150bp_50x_MT.bwa.read2.fastq
+dwgsim -C 10 -1 150 -2 150 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.fa 150bp_20x_WT
+dwgsim -C 10 -1 150 -2 150 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.TDUP.fa 150bp_20x_MT
+cat 150bp_20x_WT.bwa.read1.fastq 150bp_20x_MT.bwa.read1.fastq > 150bp_20x.read1.fastq && cat 150bp_20x_WT.bwa.read2.fastq 150bp_20x_MT.bwa.read2.fastq > 150bp_20x.read2.fastq && rm -rf 150bp_20x_WT.bwa.read1.fastq 150bp_20x_MT.bwa.read1.fastq 150bp_20x_WT.bwa.read2.fastq 150bp_20x_MT.bwa.read2.fastq
+dwgsim -C 50 -1 200 -2 200 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.fa 200bp_100x_WT
+dwgsim -C 50 -1 200 -2 200 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.TDUP.fa 200bp_100x_MT
+cat 200bp_100x_WT.bwa.read1.fastq 200bp_100x_MT.bwa.read1.fastq > 200bp_100x.read1.fastq && cat 200bp_100x_WT.bwa.read2.fastq 200bp_100x_MT.bwa.read2.fastq > 200bp_100x.read2.fastq && rm -rf 200bp_100x_WT.bwa.read1.fastq 200bp_100x_MT.bwa.read1.fastq 200bp_100x_WT.bwa.read2.fastq 200bp_100x_MT.bwa.read2.fastq
+dwgsim -C 25 -1 200 -2 200 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.fa 200bp_50x_WT
+dwgsim -C 25 -1 200 -2 200 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.TDUP.fa 200bp_50x_MT
+cat 200bp_50x_WT.bwa.read1.fastq 200bp_50x_MT.bwa.read1.fastq > 200bp_50x.read1.fastq && cat 200bp_50x_WT.bwa.read2.fastq 200bp_50x_MT.bwa.read2.fastq > 200bp_50x.read2.fastq && rm -rf 200bp_50x_WT.bwa.read1.fastq 200bp_50x_MT.bwa.read1.fastq 200bp_50x_WT.bwa.read2.fastq 200bp_50x_MT.bwa.read2.fastq
+dwgsim -C 10 -1 200 -2 200 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.fa 200bp_20x_WT
+dwgsim -C 10 -1 200 -2 200 -d 500 -s 50 -c 0 -R 0 -e 0.0005-0.003 -E 0.0005-0.003 chr20.hg19.TDUP.fa 200bp_20x_MT
+cat 200bp_20x_WT.bwa.read1.fastq 200bp_20x_MT.bwa.read1.fastq > 200bp_20x.read1.fastq && cat 200bp_20x_WT.bwa.read2.fastq 200bp_20x_MT.bwa.read2.fastq > 200bp_20x.read2.fastq && rm -rf 200bp_20x_WT.bwa.read1.fastq 200bp_20x_MT.bwa.read1.fastq 200bp_20x_WT.bwa.read2.fastq 200bp_20x_MT.bwa.read2.fastq
